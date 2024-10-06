282529453a9b52c9e35e6aaf222357e55c6c38e8
khaireddine <ihrissanek@gmail.com>
2024-10-06 02:33:53 +0100

    TP4-completed

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -4,16 +4,113 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
-
+use App\Entity\Article;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Doctrine\ORM\EntityManagerInterface;
 class IndexController extends AbstractController
+
 {
-    #[Route('/home', name: 'app_homepage')]
-    public function home(): Response
+    #[Route('/', name: 'article_list')]
+    public function home(EntityManagerInterface $entityManager): Response
     {
-        $articles = ['Artcile1', 'Article 2','Article 3'];
-        return $this->render('articles/index.html.twig', [
-            'articles'=>$articles
+        $articles = $entityManager->getRepository(Article::class)->findAll();
+        return $this->render('articles/index.html.twig', ['articles' => $articles]);
+    }
+
+    #[Route('/article/save', name: 'article_save')]
+    public function save(EntityManagerInterface $entityManager): Response
+    {
+        $article = new Article();
+        $article->setNom('Article 3');
+        $article->setPrix(3000);
+        $entityManager->persist($article);
+        $entityManager->flush();
+        return new Response('Article enregisté avec id ' . $article->getId());
+    }
+
+    #[Route('/articles/new',name:'new_article',methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager)
+    {
+        $article = new Article();
+
+        $form = $this->createFormBuilder($article)
+            ->add('nom', TextType::class)
+            ->add('prix', TextType::class)
+            ->add('save', SubmitType::class, [
+                'label' => 'Créer'
+            ])
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($article);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('article_list');
+        }
+
+        return $this->render('articles/new.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
+
+    #[Route('/article/{id}', name: 'article_show')]
+    public function show($id, EntityManagerInterface $entityManager): Response
+    {
+        $article = $entityManager->getRepository(Article::class)->find($id);
+        if (!$article) {
+            throw $this->createNotFoundException('Article not found with id ' . $id);
+        }
+        return $this->render('articles/show.html.twig', [
+            'article' => $article,
+        ]);
+    }
+
+    #[Route('/article/edit/{id}', name: 'edit_article', methods: ['GET', 'POST'])]
+    public function edit(Request $request, $id, EntityManagerInterface $entityManager): Response
+{
+    $article = $entityManager->getRepository(Article::class)->find($id);
+
+    if (!$article) {
+        throw $this->createNotFoundException('No article found for id ' . $id);
+    }
+    $form = $this->createFormBuilder($article)
+        ->add('nom', TextType::class)
+        ->add('prix', TextType::class)
+        ->add('save', SubmitType::class, [
+            'label' => 'Modifier'
+        ])
+        ->getForm();
+
+    $form->handleRequest($request);
+    if ($form->isSubmitted() && $form->isValid()) {
+        $entityManager->flush();
+        return $this->redirectToRoute('article_list');
+    }
+    return $this->render('articles/edit.html.twig', [
+        'form' => $form->createView(),
+    ]);
+}
+
+#[Route('/article/delete/{id}', name: 'delete_article', methods: ['DELETE','GET'])]
+public function delete(Request $request, $id, EntityManagerInterface $entityManager): Response
+
+{
+$article = $entityManager->getRepository(Article::class)->find($id);
+if (!$article) {
+    throw $this->createNotFoundException('Aucun article trouvé pour cet ID: ' . $id);
+}
+$entityManager->remove($article);
+$entityManager->flush();
+
+$response = new Response();
+$response->send();
+return $this->redirectToRoute('article_list');
+}
+
 }
